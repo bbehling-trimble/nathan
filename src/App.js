463d64a3ed7c688bcc657b06a6056d93b2bed0a1
Nathan Burgo <nthnbrg185@gmail.com>
2018-11-18 17:21:51 -0700

    Redux unnecessary for this project. Removed dependencies

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 // import { Provider } from 'react-redux';
 
 import './App.css';
-import store from './store.js';
 import LeafletMap from './components/LeafletMap';
 
 class App extends Component {
